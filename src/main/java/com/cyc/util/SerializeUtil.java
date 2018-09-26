3926359aed7579d41c8be79a6da3461b5f183e76
CyC2018 <1029579233@qq.com>
2018-09-26 20:53:56 +0800

    fix

--- src/main/java/com/cyc/util/SerializeUtil.java
+++ src/main/java/com/cyc/util/SerializeUtil.java
@@ -4,10 +4,12 @@ import com.cyc.pojo.Blog;
 
 public class SerializeUtil {
 
+    private static final String separator = "/////";
+
     public static String writeBlogObject(Blog blog) {
         StringBuilder s = new StringBuilder();
-        s.append(blog.getUserid()).append(",");
-        s.append(blog.getBlogid()).append(",");
+        s.append(blog.getUserid()).append(separator);
+        s.append(blog.getBlogid()).append(separator);
         s.append(DateUtil.formatDate(blog.getPublishtime())).append(",");
         s.append(blog.getContent());
         return s.toString();
@@ -15,12 +17,11 @@ public class SerializeUtil {
 
     public static Blog readBlogObject(String s) {
         Blog blog = new Blog();
-        String[] token = s.split(",");
+        String[] token = s.split(separator);
         blog.setUserid(Integer.valueOf(token[0]));
         blog.setBlogid(Integer.valueOf(token[1]));
         blog.setPublishtime(DateUtil.parseDate(token[2]));
         blog.setContent(token[3]);
         return blog;
     }
-
 }
